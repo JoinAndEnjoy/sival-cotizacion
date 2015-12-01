d4c57f70e5910fa4847ee56f27a74bfaef944bb0
ruedagato <ruedagato@gmail.com>
2015-11-30 19:10:43 -0500

    cambios interfas

--- formulario/static/formulario/js/mapa.js
+++ formulario/static/formulario/js/mapa.js
@@ -5,7 +5,11 @@
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 4.378872703630117, lng:-73.59086318750002},
-    zoom: 6
+    center: {lat: 4.7109903, lng:-74.0721436},
+    zoom: 13
   });
 }
+
+$(function () {
+	$('#fecha').datetimepicker();
+});
