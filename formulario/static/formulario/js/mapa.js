f16d966678ffb3e3dbf0d4711f8516c59b4355d9
ruedagato <ruedagato@gmail.com>
2015-12-02 17:54:36 -0500

    render mapa puntos termiando

--- formulario/static/formulario/js/mapa.js
+++ formulario/static/formulario/js/mapa.js
@@ -2,31 +2,34 @@
  * 
  */
 
- var map;
- var directionsDisplay;
- var directionsService;
+var map;
+var directionsDisplay;
+var directionsService;
 
- var markers = [];
- var markers2 = [];
- var waypts = [];
+var markers = [];
+var markers2 = [];
+var waypts = [];
+var internalmark = [];
 
- function initMap()
- {
- 	map = new google.maps.Map(document.getElementById('map'), {
- 		center: {lat: 4.7109903, lng:-74.0721436},
- 		zoom: 13,
- 		mapTypeId: google.maps.MapTypeId.ROADMAP
- 	});
- 	directionsDisplay = new google.maps.DirectionsRenderer();
- 	directionsService = new google.maps.DirectionsService();
- 	directionsDisplay.setMap(map);
+var numPun = 0;
+
+function initMap()
+{
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 4.7109903, lng:-74.0721436},
+		zoom: 13,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	directionsService = new google.maps.DirectionsService();
+	directionsDisplay.setMap(map);
 
 //	Create the search box and link it to the UI element.
-var input = document.getElementById('origen');
-var searchBox = new google.maps.places.SearchBox(input);
+	var input = document.getElementById('origen');
+	var searchBox = new google.maps.places.SearchBox(input);
 
-var input2 = document.getElementById('destino');
-var searchBox2 = new google.maps.places.SearchBox(input2);
+	var input2 = document.getElementById('destino');
+	var searchBox2 = new google.maps.places.SearchBox(input2);
 	//map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); eliminar esta linea para mantener los estilos
 
 
@@ -36,116 +39,32 @@ var searchBox2 = new google.maps.places.SearchBox(input2);
 		searchBox2.setBounds(map.getBounds());
 	});
 
-	// Listen for the event fired when the user selects a prediction and retrieve
-	// more details for that place.
-	/*
-	searchBox.addListener('places_changed', function() {
-		var places = searchBox.getPlaces();
-
-		if (places.length == 0) {
-			return;
-		}
-
-		// Clear out the old markers.
-		markers.forEach(function(marker) {
-			marker.setMap(null);
-		});
-		markers = [];
-
-		// For each place, get the icon, name and location.
-		var bounds = new google.maps.LatLngBounds();
-		places.forEach(function(place) {
-			var icon = {
-					url: place.icon,
-					size: new google.maps.Size(71, 71),
-					origin: new google.maps.Point(0, 0),
-					anchor: new google.maps.Point(17, 34),
-					scaledSize: new google.maps.Size(25, 25)
-			};
-
-			// Create a marker for each place.
-			markers.push(new google.maps.Marker({
-				map: map,
-				icon: icon,
-				title: place.name,
-				position: place.geometry.location
-			}));
-			
-			if (place.geometry.viewport) {
-				// Only geocodes have viewport.
-				bounds.union(place.geometry.viewport);
-			} else {
-				bounds.extend(place.geometry.location);
-			}
-		});
-		map.fitBounds(bounds);
-		calcRoute();
-	});*/
 	agregarListener("markers",searchBox);
 	agregarListener("markers2",searchBox2);
-	/*
-	searchBox2.addListener('places_changed', function(){
-		var places = searchBox2.getPlaces();
-		if (places.length == 0) {
-			return;
-		}
-		markers2.forEach(function(marker) {
-			marker.setMap(null);
-		});
-		markers2 = [];
-
-		var bounds = new google.maps.LatLngBounds();
-
-		places.forEach(function(place) {
-			var icon = {
-					url: place.icon,
-					size: new google.maps.Size(71, 71),
-					origin: new google.maps.Point(0, 0),
-					anchor: new google.maps.Point(17, 34),
-					scaledSize: new google.maps.Size(25, 25)
-			};
-
-			// Create a marker for each place.
-			markers2.push(new google.maps.Marker({
-				map: map,
-				icon: icon,
-				title: place.name,
-				position: place.geometry.location
-			}));
-
-			if (place.geometry.viewport) {
-				// Only geocodes have viewport.
-				bounds.union(place.geometry.viewport);
-			} else {
-				bounds.extend(place.geometry.location);
-			}
-		});
-		map.fitBounds(bounds);
-		calcRoute();
-	});*/
+	
 }
 
 function agregarListener(markersn, searchBoxn){
 	searchBoxn.addListener('places_changed', function(){
-	var places = searchBoxn.getPlaces();
-	if (places.length == 0) {
-		return;
-	}
-	window[markersn].forEach(function(marker) {
-		marker.setMap(null);
-	});
-	window[markersn] = [];
+		var places = searchBoxn.getPlaces();
+		if (places.length == 0) {
+			return;
+		}
+		window[markersn].forEach(function(marker) {
+			marker.setMap(null);
+		});
+		window[markersn] = [];
 
-	var bounds = new google.maps.LatLngBounds();
+		var bounds = new google.maps.LatLngBounds();
 
-	places.forEach(function(place) {
-		var icon = {
-			url: place.icon,
-			size: new google.maps.Size(71, 71),
-			origin: new google.maps.Point(0, 0),
-			anchor: new google.maps.Point(17, 34),
-			scaledSize: new google.maps.Size(25, 25)
-		};
+		places.forEach(function(place) {
+			var icon = {
+					url: place.icon,
+					size: new google.maps.Size(71, 71),
+					origin: new google.maps.Point(0, 0),
+					anchor: new google.maps.Point(17, 34),
+					scaledSize: new google.maps.Size(25, 25)
+			};
 
 			// Create a marker for each place.
 			window[markersn].push(new google.maps.Marker({
@@ -162,10 +81,55 @@ function agregarListener(markersn, searchBoxn){
 				bounds.extend(place.geometry.location);
 			}
 		});
-	map.fitBounds(bounds);
+		map.fitBounds(bounds);
 
-	calcRoute();
-});
+		calcRoute();
+	});
+}
+
+function agregarListenerInternal(searchBoxn,index){
+	var markersn = [];
+	var indice = index;
+	searchBoxn.addListener('places_changed', function(){
+		var places = searchBoxn.getPlaces();
+		if (places.length == 0) {
+			return;
+		}
+		markersn.forEach(function(marker) {
+			marker.setMap(null);
+		});
+		markersn = [];
+
+		var bounds = new google.maps.LatLngBounds();
+
+		places.forEach(function(place) {
+			var icon = {
+					url: place.icon,
+					size: new google.maps.Size(71, 71),
+					origin: new google.maps.Point(0, 0),
+					anchor: new google.maps.Point(17, 34),
+					scaledSize: new google.maps.Size(25, 25)
+			};
+
+			// Create a marker for each place.
+				markersn.push(new google.maps.Marker({
+				map: map,
+				icon: icon,
+				title: place.name,
+				position: place.geometry.location
+			}));
+
+			if (place.geometry.viewport) {
+				// Only geocodes have viewport.
+				bounds.union(place.geometry.viewport);
+			} else {
+				bounds.extend(place.geometry.location);
+			}
+		});
+		map.fitBounds(bounds);
+		internalmark.splice(indice,1,markersn[0])
+		calcRoute();
+	});
 }
 
 
@@ -173,30 +137,46 @@ function calcRoute() {
 	if (markers.length == 0 || markers2.length == 0){
 		return;
 	}
-		
 	
-	waypts.push({
-		location: {lat: 5.205117999999999, lng: -74.73708099999999},
-		stopover: true
+	waypts = [];
+	internalmark.forEach(function(punto){
+		waypts.push({
+			location: punto.position,
+			stopover: true
+		});
 	});
 	var start = markers[0].position;
 	var end = markers2[0].position;
 	var request = {
-		origin:start,
-		destination:end,
-		waypoints: waypts,
-		optimizeWaypoints: true,
-		travelMode: google.maps.TravelMode.DRIVING,
-		provideRouteAlternatives:true
+			origin:start,
+			destination:end,
+			waypoints: waypts,
+			optimizeWaypoints: true,
+			travelMode: google.maps.TravelMode.DRIVING,
+			provideRouteAlternatives:true
 	};
 	directionsService.route(request, function(result, status) {
 		if (status == google.maps.DirectionsStatus.OK) {
 			directionsDisplay.setDirections(result);
-			map.clearOverlays();
+			eliminarMarks();
 		}
 	});
 }
 
+function eliminarMarks()
+{
+	markers.forEach(function(marker) {
+		marker.setMap(null);
+	});
+	
+	markers2.forEach(function(marker) {
+		marker.setMap(null);
+	});
+	
+	internalmark.forEach(function(marker) {
+		marker.setMap(null);
+	});
+}
 
 $("#fecha").focus(function(){
 	$("#icono-fecha").css("color","#2e3436");
@@ -207,16 +187,27 @@ $("#fecha").focusout(function(){
 });
 
 $('#definir-ruta').click(function(){
-	$('#puntos-div').css("display","block");
-	$('#puntos-div').append("<div class = 'punto-int'>\
-								<div class='inner-addon left-addon'>\
-								<input type='text' name'origen' id='origen'\
-								placeholder='Punto de origen' class='botones-fomulario punto-int-input' required>\
-								<i class='glyphicon glyphicon-map-marker mis-iconos'></i> \
-							</div>");
+	numPun++;
+	if(numPun<9)
+	{
+		$('#puntos-div').css("display","block");
+		$('#puntos-div').append("<div class = 'punto-int'>\
+				<div class='inner-addon left-addon'>\
+				<input type='text' name='origen' id='intermedio"+numPun+"'\
+				placeholder='Punto intermedio "+numPun+"' class='botones-fomulario punto-int-input' required>\
+				<i class='glyphicon glyphicon-map-marker mis-iconos'></i> \
+		</div>");
 
-	$('#puntos-div').animate({
-          scrollTop: 1000
-        }, 1000);
+		$('#puntos-div').animate({
+			scrollTop: 1000
+		}, 600);
+		var input = document.getElementById('intermedio'+numPun);
+		var searchBox = new google.maps.places.SearchBox(input);
+		agregarListenerInternal(searchBox,numPun-1);
+	}
+	else
+	{
+		alert("No se pueden definir más puntos")
+	}
 });
 
