6e6bd49a829b434d37ae0706204830c09b51a2cd
ruedagato <ruedagato@gmail.com>
2015-12-17 20:18:38 -0500

    tabla terminada

--- administrador/static/administrador/js/propio.js
+++ administrador/static/administrador/js/propio.js
@@ -6,12 +6,15 @@
 
 
 $(document).ready(function () {
-    
+
     $('#tabla-datos').DataTable(
             {
                 "ajax": {
                     "url": "/administrador/json/",
                     "type": "GET"
+                },
+                "language": {
+                    "url": "http://cdn.datatables.net/plug-ins/1.10.10/i18n/Spanish.json"
                 },
                 "aoColumns": [
                     {"mData": 'nombre'},
@@ -19,7 +22,13 @@ $(document).ready(function () {
                     {"mData": 'origen'},
                     {"mData": 'destino'},
                     {"mData": 'camino'},
-                    {"mData": 'distancia'}
+                    {"mData": 'distancia'},
+                    {"mData": null,
+                        "bSortable": false,
+                        "mRender": function (o) {
+                            return '<button class="btn m-b-xs w-xs btn-default btn-rounded">Ver mapa</button> <button class="btn m-b-xs w-xs btn-success btn-rounded">Responder</button>';
+                        }
+                    }
                 ]
             });
 
